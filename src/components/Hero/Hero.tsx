c6c51f94ab88701eeae9455df2ab32135fa34d44
Vladislav <haos1996haos@gmail.com>
2022-10-11 22:46:21 +0300

    create hero and animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Hero.scss";
-import faceHero from "../../assets/img/faceHero.png";
-import mokingHero from "../../assets/img/mokingHero.png";
-import brailleHero from "../../assets/img/brailleHero.png";
+import faceHero from "../../assets/img/hero/faceHero.png";
+import mokingHero from "../../assets/img/hero/mokingHero.png";
+import brailleHero from "../../assets/img/hero/brailleHero.png";
+import malan from "../../assets/img/hero/title.svg";
 import { Button } from "../Button/Button";
 
 export const Hero: React.FC = () => {
@@ -10,20 +11,32 @@ export const Hero: React.FC = () => {
     <main>
       <section className="hero">
         <div className="container">
-          <div className="hero__title-block">
-            <h1 className="hero__title">MALAN</h1>
-            <span className="hero__span">tattoo</span>
-          </div>
-          <div className="hero__content">
-            <img src={faceHero} alt="Тату пришельцы" />
-            <img src={mokingHero} alt="Тату издеваешься" />
-            <div className="hero__button-block">
+          <div className="hero__container">
+            <div className="hero__title-block">
+              <h1 className="hero__title">
+                <img src={malan} alt="Заголовок" />
+              </h1>
+              <span className="hero__span">tattoo</span>
+            </div>
+            <div className="hero__content">
               <img
-                className="hero__img-braille"
-                src={brailleHero}
-                alt="Тату Брайля"
+                className="hero__img-face"
+                src={faceHero}
+                alt="Тату пришельцы"
               />
-              <Button title={"Записаться"} />
+              <img
+                className="hero__img-moking"
+                src={mokingHero}
+                alt="Тату издеваешься"
+              />
+              <div className="hero__button-block">
+                <img
+                  className="hero__img-braille"
+                  src={brailleHero}
+                  alt="Тату Брайля"
+                />
+                <Button title={"Записаться"} />
+              </div>
             </div>
           </div>
         </div>
