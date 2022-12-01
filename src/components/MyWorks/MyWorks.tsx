295edbc45003a62d79f6101c0c53e98beb1937db
EmelyanovVlad <haos1996haos@gmail.com>
2022-12-01 21:47:03 +0300

    create list my works

--- src/components/MyWorks/MyWorks.tsx
+++ src/components/MyWorks/MyWorks.tsx
@@ -1,5 +1,25 @@
 import { Title } from '../Title/Title';
+import './MyWorks.scss';
 
 export const MyWorks: React.FC = () => {
-  return <Title title={'my works'} color={'blue'} row={[2, 1]} />;
+  const clickWorks = (item: String) => {
+    console.log(item);
+  };
+  const myWorks = ['Тату', 'Зажившие тату', 'Эскизы'];
+  return (
+    <>
+      <Title title={'my works'} color={'blue'} row={[2, 1]} />
+      <div className='container'>
+        <ul className='works__list'>
+          {myWorks.map((item, i) => (
+            <li className='works__item' key={i}>
+              <a onClick={() => clickWorks(item)} className='works__link'>
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
