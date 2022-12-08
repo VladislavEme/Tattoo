fc2f8cb00838281a2531ada57430a078fdcf2319
Vlad Em <haos1996haos@gmail.com>
2022-12-08 18:18:07 +0300

    edited gallery slider

--- src/components/Sections/MyWorks/MyWorks.tsx
+++ src/components/Sections/MyWorks/MyWorks.tsx
@@ -11,7 +11,9 @@ import { WorksNav } from '../../WorksNav/WorksNav';
 import imgData from '../../../assets/img/worksImg.json';
 
 export const MyWorks: React.FC = () => {
-  const openGallery = useSelector((state: RootState) => state.gallery.openGallery);
+  const openGallery = useSelector(
+    (state: RootState) => state.gallery.openGallery
+  );
   const dispatch = useDispatch();
 
   const clickAllWorks = () => {
@@ -22,41 +24,50 @@ export const MyWorks: React.FC = () => {
   const chevronWidth = 150;
 
   return (
-    <section className='works'>
-      <h2 className='works__title'>
+    <section className="works">
+      <h2 className="works__title">
         <Title title={'my works'} color={'blue'} row={[2, 1]} />
       </h2>
-      <div className='container'>
-        <div className='works__nav'>
+      <div className="container">
+        <div className="works__nav">
           <WorksNav />
         </div>
-     
+
         <ItemsCarousel
-          classes={{  itemWrapper:'img-wrapper', wrapper:'wrapper-slider' }}
+          classes={{ itemWrapper: 'img-wrapper', wrapper: 'wrapper-slider' }}
           requestToChangeActive={setActiveItemIndex}
           activeItemIndex={activeItemIndex}
           numberOfCards={3}
           gutter={30}
-          leftChevron={<button className='button__slider'>&#8249;</button>}
-          rightChevron={<button className='button__slider'>&#8250;</button>}
+          leftChevron={<button className="button__slider">&#8249;</button>}
+          rightChevron={<button className="button__slider">&#8250;</button>}
           outsideChevron
           slidesToScroll={1}
           chevronWidth={chevronWidth}
         >
           {imgData.map((item, i) => (
             <img
-              className={activeItemIndex + 1 === i ? 'img__slider-center' : activeItemIndex===i?'img__slider-left':activeItemIndex+2===i?'img__slider-right':'img__slider'}
+              className={
+                activeItemIndex + 1 === i
+                  ? 'img__slider-center'
+                  : activeItemIndex === i
+                  ? 'img__slider-left'
+                  : activeItemIndex + 2 === i
+                  ? 'img__slider-right'
+                  : 'img__slider'
+              }
               src={item}
               alt={`Фото № ${i}`}
               key={i}
             />
           ))}
         </ItemsCarousel>
+        <div className="works__button">
+          <Button clickButton={clickAllWorks} title={'Показать все'} />
+        </div>
 
         {openGallery && <ModalGallery />}
       </div>
-
-      <Button clickButton={clickAllWorks} title={'Показать все'} />
     </section>
   );
 };
